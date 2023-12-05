1ef0ed3aeb2640d44f74e89e46d701ed127e3c51
isamaulanatantra <isamaulanatantra@gmail.com>
2023-12-05 11:09:07 +0700

    app

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,7 +29,7 @@ class AppServiceProvider extends ServiceProvider
 
         if (env('APP_ENV') === 'production') {
             \Illuminate\Support\Facades\URL::forceScheme('https');
-        } else {
+        } else if (env('APP_ENV') === 'local') {
             \Illuminate\Support\Facades\URL::forceScheme('http');
         }
     }
