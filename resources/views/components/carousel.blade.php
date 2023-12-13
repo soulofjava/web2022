ab712ce869115ab40f9a2f9cc7ff9ba2aa2ad653
isamaulanatantra <isamaulanatantra@gmail.com>
2023-12-13 10:49:38 +0700

    ccc

--- resources/views/components/carousel.blade.php
+++ resources/views/components/carousel.blade.php
@@ -1,7 +1,6 @@
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
     <div class="carousel-inner">
         @forelse($jjj->gambar as $gambar)
-
         @if($loop->iteration == 1)
         <div class="carousel-item active">
             <img src="{{  url('storage/')}}/{{ $gambar->path }}" class="d-block w-100" alt="{{ $gambar->file_name }}">
@@ -10,6 +9,7 @@
         <div class="carousel-item">
             <img src="{{ url('storage/')}}/{{ $gambar->path }}" class="d-block w-100" alt="{{ $gambar->file_name }}">
         </div>
+        @endif
         @empty
         <div class="carousel-item active">
             <img src="{{ asset('img/soulofjava.jpg') }}" class="d-block w-100" alt="soul of java">
