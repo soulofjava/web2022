5ee15d80e2e5f43114ced89d11a57de335a79d43
isamaulanatantra <isamaulanatantra@gmail.com>
2024-01-08 13:03:22 +0700

    hapus css tidak perlu

--- resources/views/front/pages/globalsearch.blade.php
+++ resources/views/front/pages/globalsearch.blade.php
@@ -1,7 +1,7 @@
 @extends('front.layouts.app')
 @push('after-style')
-<link rel="stylesheet" type="text/css"
-    href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+<!-- <link rel="stylesheet" type="text/css"
+    href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css"> -->
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
 <style>
     .pagination>li.active>a {
