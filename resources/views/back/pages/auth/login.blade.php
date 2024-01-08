935de4f60b6f2670f3a5c985b5c8c116d204d7ae
isamaulanatantra <isamaulanatantra@gmail.com>
2024-01-08 10:53:26 +0700

    halaman login

--- resources/views/back/pages/auth/login.blade.php
+++ resources/views/back/pages/auth/login.blade.php
@@ -14,8 +14,8 @@
             </a>
           </div>
           <!-- /Logo -->
-          <h4 class="mb-2">Welcome to Sneat! 👋</h4>
-          <p class="mb-4">Please sign-in to your account and start the adventure</p>
+          <!-- <h4 class="mb-2">Welcome to Sneat! 👋</h4> -->
+          <!-- <p class="mb-4">Please sign-in to your account and start the adventure</p> -->
           <form id="formAuthentication" class="mb-3" action="{{ route('login') }}" method="POST">
             @csrf
             <div class="mb-3">
