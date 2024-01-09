7fb6c4c43498718e5d1431a2ebf6a4be7337d345
isamaulanatantra <isamaulanatantra@gmail.com>
2024-01-09 09:08:58 +0700

    app

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (env('APP_ENV') != 'local') {
+            $this->app['request']->server->set('HTTPS', true);
+        }
+
         Paginator::useBootstrap();
     }
 }
