be1dc19ce734a4b7f1cf1949b113fe9b70886221
isamaulanatantra <57425459+soulofjava@users.noreply.github.com>
2024-01-24 10:10:48 +0700

    Update file-manager.php

--- config/file-manager.php
+++ config/file-manager.php
@@ -103,7 +103,7 @@ return [
      * Add your middleware name to array -> ['web', 'auth', 'admin']
      * !!!! RESTRICT ACCESS FOR NON ADMIN USERS !!!!
      */
-    'middleware'        => ['web'],
+    'middleware'        => ['web', 'auth'],
 
     /***************************************************************************
      * ACL mechanism ON/OFF
