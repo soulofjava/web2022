0bcd8bff7250ffad59f986e43a858ce116a20af4
isamaulanatantra <isamaulanatantra@gmail.com>
2024-02-07 16:30:10 +0700

    hapus side

--- resources/views/front/pages/page.blade.php
+++ resources/views/front/pages/page.blade.php
@@ -23,7 +23,7 @@
 <section class="blog-details-area py-130 rpy-100">
     <div class="container">
         <div class="row">
-            <div class="col-lg-8">
+            <div class="col">
                 <div class="blog-details-wrap">
                     @if($data->menu_name == 'Daftar Informasi Publik')
                     <x-jip />
@@ -32,41 +32,6 @@
                     @else
                     {!! $data->content !!}
                     @endif
-                </div>
-            </div>
-            <div class="col-lg-4">
-                <div class="blog-sidebar rmt-75">
-                    <div class="widget widget-search wow fadeInUp delay-0-2s">
-                        {{Form::open(['route' => 'global.search','method' => 'get', ''])}}
-                        {{Form::text('kolomcari', null,['class' => '',
-                        'placeholder' => 'Kata Pencarian','id'=>'textareaID1'])}}
-                        <button type="submit"><i class="searchbutton fa fa-search"></i></button>
-                        {{Form::close()}}
-                    </div>
-                    <div class="widget widget-recent-courses wow fadeInUp delay-0-2s">
-                        <h4 class="widget-title">Postingan Terpopular</h4>
-                        <ul>
-                            @foreach($news as $item)
-                            <li>
-                                @if($item->gambarmuka)
-                                <div class="image">
-                                    <img src="{{ route('helper.show-picture', ['path' => $item->gambarmuka->path]) }}" alt="Course">
-                                </div>
-                                @else
-                                <div class="image">
-                                    <img src="{{ asset('assets/bkdwonosobo.png') }}" alt="Course">
-                                </div>
-                                @endif
-                                <div class="content">
-                                    <h6><a href="{{ url('/news-detail', $item->slug) }}">{{
-                                            \Illuminate\Support\Str::limit($item->title, 50, $end='...') }}</a></h6>
-                                    <span>By <a href="#">{{ $item->uploader->name
-                                            }}</a></span>
-                                </div>
-                            </li>
-                            @endforeach
-                        </ul>
-                    </div>
                 </div>
             </div>
         </div>
