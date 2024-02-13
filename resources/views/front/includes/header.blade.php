3b0c2e4166d2992e189ad87f225b855da9a24178
isamaulanatantra <isamaulanatantra@gmail.com>
2024-02-13 11:27:55 +0700

    pindah title

--- resources/views/front/includes/header.blade.php
+++ resources/views/front/includes/header.blade.php
@@ -1,13 +1,6 @@
 <!-- ======= Header ======= -->
 <header id="header" class="header d-flex align-items-center">
     <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
-
-        <a href="{{ url('/') }}" class="logo d-flex align-items-center">
-            <!-- Uncomment the line below if you also wish to use an image logo -->
-            <!-- <img src="assets/img/logo.png" alt=""> -->
-            <h1>{{ $data_website->web_name }}<span>.</span></h1>
-        </a>
-
         <i class="mobile-nav-toggle mobile-nav-show bi bi-list"></i>
         <i class="mobile-nav-toggle mobile-nav-hide d-none bi bi-x"></i>
         <nav id="navbar" class="navbar">
@@ -124,7 +117,11 @@
                 </li>
             </ul>
         </nav>
-        <!-- .navbar -->
+        <a href="{{ url('/') }}" class="logo d-flex align-items-center">
+            <!-- Uncomment the line below if you also wish to use an image logo -->
+            <!-- <img src="assets/img/logo.png" alt=""> -->
+            <h1>{{ $data_website->web_name }}<span>.</span></h1>
+        </a>
     </div>
 </header>
 <!-- End Header -->
