f8aa6d11c6e98b0416e81f11277bf7e9be26e151
isamaulanatantra <isamaulanatantra@gmail.com>
2024-02-13 10:55:02 +0700

    str replace

--- app/Models/FrontMenu.php
+++ app/Models/FrontMenu.php
@@ -11,6 +11,13 @@ class FrontMenu extends Model
     use HasFactory, SoftDeletes;
     protected $guarded = [];
 
+    public function getContentAttribute($value)
+    {
+        // Ganti URL 'src' menggunakan regular expression
+        $newValue = str_replace('src="https://website.wonosobokab.go.id/storage/', 'src="' . url('show-picture?path='), $value);
+        return $newValue;
+    }
+
     public function menu_induk()
     {
         return $this->belongsTo(FrontMenu::class, 'menu_parent');
