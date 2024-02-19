cfc7107b6bf80f67cd37fdf7dbaa2113b13b8593
isamaulanatantra <isamaulanatantra@gmail.com>
2024-02-19 15:39:30 +0700

    db

--- database/migrations/2024_02_19_083751_create_table_kategori.php
+++ database/migrations/2024_02_19_083751_create_table_kategori.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('table_kategori', function (Blueprint $table) {
+        Schema::table('kategoris', function (Blueprint $table) {
             $table->id();
             $table->string('kategori_kelas');
             $table->string('type');
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('table_kategori');
+        Schema::dropIfExists('kategoris');
     }
 };
