4eef94fc69e9b802f1348662133ba948d4e75354
isamaulanatantra <isamaulanatantra@gmail.com>
2024-02-20 08:34:27 +0700

    lorem

--- resources/views/front/arsha/pages/elearning.blade.php
+++ resources/views/front/arsha/pages/elearning.blade.php
@@ -21,9 +21,9 @@
 
             <div class="section-title">
                 <h2>Daftar Kelas</h2>
-                <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint
+                <!-- <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint
                     consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia
-                    fugiat sit in iste officiis commodi quidem hic quas.</p>
+                    fugiat sit in iste officiis commodi quidem hic quas.</p> -->
             </div>
 
         </div>
