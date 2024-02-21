ff20a3470b70659e3a7da96cf9dbc29d3c7960ca
isamaulanatantra <isamaulanatantra@gmail.com>
2024-02-21 11:55:33 +0700

    caro

--- resources/views/front/pages/index.blade.php
+++ resources/views/front/pages/index.blade.php
@@ -2,6 +2,24 @@
 @push('after-style')
 <!-- Bootstrap CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<style>
+    .card {
+        position: relative;
+    }
+
+    .card-overlay {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        text-align: center;
+        color: white;
+        width: 100%;
+        /* background-color: rgba(0, 0, 0, 0.5); */
+        /* Ubah opacity sesuai kebutuhan */
+        padding: 20px;
+    }
+</style>
 @endpush
 @section('content')
 <div class="layout-content-1 layout-item-3-1 search-pad grid-limit">
@@ -10,92 +28,44 @@
         <center>
             <h2>{{ $t->name }}</h2>
         </center>
-        <div id="myCarousel{{ $loop->iteration }}" class="carousel slide" data-ride="carousel">
+        <div id="multi-item-example{{ $loop->iteration }}" class="carousel slide" data-ride="carousel"
+            style="height: auto;">
             <div class="carousel-inner">
-                @foreach(App\Models\News::with('gambarmuka')->withAnyTag($t->name)->where('publish',1)->latest('date')->get()
+                @foreach(App\Models\News::with('gambarmuka')->withAnyTag($t->name)->where('publish',
+                1)->latest('date')->get()->chunk(3)
                 as $it)
-                @if($loop->first)
-                <div class="carousel-item active">
-                    <a href="{{ url('news-detail', $it->slug) }}">
-                        <img src="{{ asset('Green and Black Maximalist Action  Adventure YouTube Channel Art.png') }}"
-                            alt="Slide 1" width="100%" height="300px">
-                        <div class="carousel-caption">
-                            <h3 style="color: white;">
-                                {{ $it->title }}
-                            </h3>
-                            <p style="color: white;">
-                                {{ \Carbon\Carbon::parse($it->date)->format('l') }}, {{
-                                \Carbon\Carbon::parse( $it->date
-                                )->toFormattedDateString() }}
-                            </p>
+                <div class="carousel-item {{ ($loop->first) ? 'active' : ''}}">
+                    <div class="row">
+                        @foreach($it as $i)
+                        <div class="col-md-4 mb-1">
+                            <a href="{{ url('news-detail', $i['slug']) }}">
+                                <div class="card">
+                                    <img src="{{ asset('hand-painted-watercolor-background-with-sky-clouds-shape.jpg') }}"
+                                        class="card-img-top" alt="img" height="200px">
+                                    <div class="card-overlay">
+                                        <h5 class="card-title">{{ $i['title'] }}</h5>
+                                        <p class="card-text">{{ $i['date'] }}</p>
+                                    </div>
+                                </div>
+                            </a>
                         </div>
-                    </a>
+                        @endforeach
+                    </div>
                 </div>
-                @else
-                <div class="carousel-item">
-                    <a href="{{ url('news-detail', $it->slug) }}">
-                        <img src="{{ asset('Green and Black Maximalist Action  Adventure YouTube Channel Art.png') }}"
-                            alt="Slide 1" width="100%" height="300px">
-                        <div class="carousel-caption">
-                            <h3 style="color: white;">
-                                {{ $it->title }}
-                            </h3>
-                            <p style="color: white;">
-                                {{ \Carbon\Carbon::parse($it->date)->format('l') }}, {{
-                                \Carbon\Carbon::parse( $it->date
-                                )->toFormattedDateString() }}
-                            </p>
-                        </div>
-                    </a>
-                </div>
-                @endif
                 @endforeach
             </div>
-
-            <!-- Left and right controls -->
-            <a class="carousel-control-prev" href="#myCarousel{{ $loop->iteration }}" data-slide="prev">
-                <span class="carousel-control-prev-icon"></span>
+            <a class="carousel-control-prev" href="#multi-item-example{{ $loop->iteration }}" role="button"
+                data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
             </a>
-            <a class="carousel-control-next" href="#myCarousel{{ $loop->iteration }}" data-slide="next">
-                <span class="carousel-control-next-icon"></span>
+            <a class="carousel-control-next" href="#multi-item-example{{ $loop->iteration }}" role="button"
+                data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
             </a>
         </div>
         @endforeach
-        <!-- @foreach(Conner\Tagging\Model\Tag::all() as $t)
-        <div id="myCarousel{{ $loop->iteration }}" class="carousel slide" data-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <div class="container">
-                        <div class="row">
-                            <h3 class="text-center w-100">{{ $t->name }}</h3>
-                            @foreach(App\Models\News::with('gambarmuka')->withAnyTag($t->name)->get() as $it)
-                            <div class="col-sm-4">
-                                <div class="card">
-                                    <div class="card-body">
-                                        <h5 class="card-title">
-                                            {{ $it->title }}
-                                        </h5>
-                                        <p class="card-text">
-                                            {{ \Carbon\Carbon::parse($it->date)->format('l') }}, {{
-                                            \Carbon\Carbon::parse( $it->date
-                                            )->toFormattedDateString() }}
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                            @endforeach
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <a class="carousel-control-prev" href="#myCarousel{{ $loop->iteration }}" data-slide="prev">
-                <span class="carousel-control-prev-icon"></span>
-            </a>
-            <a class="carousel-control-next" href="#myCarousel{{ $loop->iteration }}" data-slide="next">
-                <span class="carousel-control-next-icon"></span>
-            </a>
-        </div>
-        @endforeach -->
     </div>
     <div class="layout-sidebar layout-item gutter-medium">
         <!-- WIDGET SIDEBAR -->
