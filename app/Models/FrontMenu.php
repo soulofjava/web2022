d02abfa26aaf06235f7a6a0079a41541ca1ca3a3
isamaulanatantra <isamaulanatantra@gmail.com>
2024-03-18 12:05:38 +0700

    pdf view

--- app/Models/FrontMenu.php
+++ app/Models/FrontMenu.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class FrontMenu extends Model
 {
@@ -13,9 +14,16 @@ class FrontMenu extends Model
 
     public function getContentAttribute($value)
     {
-        // Ganti URL 'src' menggunakan regular expression
-        $newValue = str_replace('src="https://website.wonosobokab.go.id/storage/', 'src="' . url('show-picture?path='), $value);
-        return $newValue;
+        if (Str::startsWith($value, 'src="https://website.wonosobokab.go.id/storage/')) {
+            $newValue = str_replace('src="https://website.wonosobokab.go.id/storage/', 'src="https://docs.google.com/viewer?url="' . url('show-picture?path='), $value);
+        } else {
+            $newValue = str_replace('src="', 'src="https://docs.google.com/viewer?url=', $value);
+        }
+
+        $iframeEmbed = str_replace('<embed', '<iframe', $newValue);
+        $iframeEmbed = str_replace('></embed>', '></iframe>', $iframeEmbed);
+
+        return $iframeEmbed;
     }
 
     public function menu_induk()
