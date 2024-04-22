bdeb7c42c21152bc09731d7839aa436425fe23bc
isamaulanatantra <isamaulanatantra@gmail.com>
2024-04-22 10:52:28 +0700

    struktur

--- resources/views/components/personil.blade.php
+++ resources/views/components/personil.blade.php
@@ -13,7 +13,7 @@
                 </tr>
             </thead>
             <tbody>
-                @foreach ($kadis as $list)
+                @foreach ($struktural as $list)
                 <tr>
                     <td>{{ $list->gdp.$list->nama.', '.$list->gdb }}</td>
                     <td>{{ $list->pangkat.' / '.$list->golru }}</td>
@@ -21,7 +21,7 @@
                     <td>{{ $list->pendidikan }}</td>
                 </tr>
                 @endforeach
-                @foreach ($sekdin as $list)
+                @foreach ($non_struktural as $list)
                 <tr>
                     <td>{{ $list->gdp.$list->nama.', '.$list->gdb }}</td>
                     <td>{{ $list->pangkat.' / '.$list->golru }}</td>
@@ -29,31 +29,6 @@
                     <td>{{ $list->pendidikan }}</td>
                 </tr>
                 @endforeach
-                @foreach ($kabid_informatika as $list)
-                <tr>
-                    <td>{{ $list->gdp.$list->nama.', '.$list->gdb }}</td>
-                    <td>{{ $list->pangkat.' / '.$list->golru }}</td>
-                    <td>{{ $list->jabatannya }}</td>
-                    <td>{{ $list->pendidikan }}</td>
-                </tr>
-                @endforeach
-                @foreach ($kabid_ikp as $list)
-                <tr>
-                    <td>{{ $list->gdp.$list->nama.', '.$list->gdb }}</td>
-                    <td>{{ $list->pangkat.' / '.$list->golru }}</td>
-                    <td>{{ $list->jabatannya }}</td>
-                    <td>{{ $list->pendidikan }}</td>
-                </tr>
-                @endforeach
-                @foreach ($staff_diskominfo as $list)
-                <tr>
-                    <td>{{ $list->gdp.$list->nama.', '.$list->gdb }}</td>
-                    <td>{{ $list->pangkat.' / '.$list->golru }}</td>
-                    <td>{{ $list->jabatannya }}</td>
-                    <td>{{ $list->pendidikan }}</td>
-                </tr>
-                @endforeach
-    
             </tbody>
         </table>
     </div>
