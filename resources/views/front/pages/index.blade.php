b13487729e53bcff1f5b6a1353b7157f693cc787
isamaulanatantra <isamaulanatantra@gmail.com>
2024-04-22 08:11:29 +0700

    informasi umum

--- resources/views/front/pages/index.blade.php
+++ resources/views/front/pages/index.blade.php
@@ -194,6 +194,69 @@
 </section>
 <!-- Coach Section End -->
 
+<!-- Coach Section Start -->
+<section class="coach-section rel z-1 pt-120 rpt-90 pb-100 rpb-70 bg-lighter">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xl-6 col-lg-7 col-md-8">
+                <div class="section-title text-center mb-40">
+                    <h2>Informasi Umum</h2>
+                </div>
+            </div>
+        </div>
+        <!-- <ul class="coach-filter mb-35">
+            <li data-filter="*" class="current">Show All</li>
+            <li data-filter=".design">Web Design</li>
+            <li data-filter=".marketing">Marketing</li>
+            <li data-filter=".development">Development</li>
+            <li data-filter=".technology">IT & Technology</li>
+            <li data-filter=".photography">Photography</li>
+        </ul> -->
+        <div class="row coach-active justify-content-center">
+            @foreach(App\Models\News::where('tag',
+            'informasi umum')->latest('date')->get() as $iu)
+            <div class="col-lg-3 col-md-6 item marketing technology">
+                <div class="coach-item wow fadeInUp delay-0-2s">
+                    <div class="coach-image">
+                        <a href="{{ url('/news-detail', $iu->slug) }}">
+                            @if($iu->gambarmuka)
+                            <img src="{{ route('helper.show-picture', ['path' => $iu->gambarmuka->path]) }}"
+                                class="img-thumbnail" alt="{{ $iu->gambarmuka->file_name }}"
+                                style="object-fit: cover; width: 270px; height: 148px">
+                            @else
+                            <img src="{{ asset('assets/bkdwonosobo.png') }}" alt="Course">
+                            @endif
+                        </a>
+                        <!-- <img src="{{ asset('assets/front/images/coachs/coach1.jpg') }}" alt="Coach"> -->
+                    </div>
+                    <div class="coach-content" style="height: 175px;">
+                        <!-- <span class="label">Basic Coach</span> -->
+                        <h4><a href="{{ url('/news-detail', $iu->slug) }}">{{ Str::limit($iu->title, 40, '...') }}</a>
+                        </h4>
+                        <!-- <div class="ratting-price">
+                            <div class="ratting">
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <i class="fas fa-star"></i>
+                                <span>(3k)</span>
+                            </div>
+                            <span class="price">256.95</span>
+                        </div> -->
+                        <!-- <ul class="coach-footer">
+                            <li><i class="far fa-file-alt"></i><span>12 Lessions</span></li>
+                            <li><i class="far fa-user"></i><span>seats</span></li>
+                        </ul> -->
+                    </div>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+</section>
+<!-- Coach Section End -->
+
 <!-- Work Process Section Start -->
 <!-- <section class="work-process-section bg-white rel z-1 pt-130 rpt-100 pb-100 rpb-70">
     <div class="container">
