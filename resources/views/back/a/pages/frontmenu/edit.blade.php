be3c685a9a1dd3fb0064e2f047262fa703280a09
isamaulanatantra <isamaulanatantra@gmail.com>
2024-04-22 14:57:02 +0700

    editform

--- resources/views/back/a/pages/frontmenu/edit.blade.php
+++ resources/views/back/a/pages/frontmenu/edit.blade.php
@@ -24,9 +24,8 @@
                         </div>
                         <div class="form-group label-floating">
                             <label class="control-label">Content</label>
-                            {{Form::textarea('description', null,['class' => 'my-editor
+                            {{Form::textarea('content', null,['class' => 'my-editor
                             form-control','id'=>'my-editor'])}}
-
                         </div>
                         <div class="d-flex text-right">
                             <a href="{{ route('frontmenu.index') }}" class="btn btn-default btn-fill">Cancel</a>
