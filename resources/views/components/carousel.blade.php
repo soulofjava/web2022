792c1bb27d505922a2a9e518198cfca5961864f5
isamaulanatantra <isamaulanatantra@gmail.com>
2024-04-29 09:55:49 +0700

    carousel

--- resources/views/components/carousel.blade.php
+++ resources/views/components/carousel.blade.php
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-=======
-<div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
->>>>>>> 57cd9d6f8615469020dc8a6e5e8bddd03a11010e
     <div class="carousel-inner">
         @forelse($jjj->gambar as $gambar)
 
@@ -10,18 +6,11 @@
 
         @if(Str::contains($gambar->path, 'https'))
         <div class="carousel-item active">
-<<<<<<< HEAD
             <img src="{{ route('helper.show-picture', ['path' => $gambar->path]) }}" class="d-block w-100"
                 alt="{{ $gambar->file_name }}">
         </div>
         @else
         <div class="carousel-item">
-=======
-            <img src="{{ $gambar->path }}" class="d-block w-100" alt="{{ $gambar->file_name }}">
-        </div>
-        @else
-        <div class="carousel-item active">
->>>>>>> 57cd9d6f8615469020dc8a6e5e8bddd03a11010e
             <img src="{{ route('helper.show-picture', ['path' => $gambar->path]) }}" class="d-block w-100"
                 alt="{{ $gambar->file_name }}">
         </div>
@@ -33,7 +22,8 @@
         </div>
         @else
         <div class="carousel-item">
-            <img src="{{ route('helper.show-picture', ['path' => $gambar->path]) }}" class="d-block w-100" alt="{{ $gambar->file_name }}">
+            <img src="{{ route('helper.show-picture', ['path' => $gambar->path]) }}" class="d-block w-100"
+                alt="{{ $gambar->file_name }}">
         </div>
         @endif
 
