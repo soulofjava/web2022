749d46eb2b4c423e01440d2edffee85928991a55
isamaulanatantra <isamaulanatantra@gmail.com>
2024-04-30 08:43:32 +0700

    fix bug coment

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -5,11 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-<<<<<<<< HEAD:app/Models/Download.php
-class Download extends Model
-========
 class Comment extends Model
->>>>>>>> 57cd9d6f8615469020dc8a6e5e8bddd03a11010e:app/Models/Comment.php
 {
     use HasFactory;
     protected $guarded = [];
