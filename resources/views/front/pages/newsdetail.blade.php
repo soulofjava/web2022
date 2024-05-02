94108d31f877a027a2c96507426640a646872d05
isamaulanatantra <isamaulanatantra@gmail.com>
2024-05-02 13:00:48 +0700

    ganti image

--- resources/views/front/pages/newsdetail.blade.php
+++ resources/views/front/pages/newsdetail.blade.php
@@ -4,7 +4,7 @@
 
     <!-- ======= Breadcrumbs ======= -->
     <div class="breadcrumbs d-flex align-items-center"
-        style="background-image: url('assets/front/assets/img/breadcrumbs-bg.jpg');">
+        style="background-image: url('{{ asset('assets/front/assets/img/breadcrumbs-bg.jpg') }}');">
         <div class="container position-relative d-flex flex-column align-items-center" data-aos="fade">
 
             <!-- <h2>Blog Details</h2> -->
