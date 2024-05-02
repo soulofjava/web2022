e07f786483b3ee367803027e8a07961b35961cd4
isamaulanatantra <isamaulanatantra@gmail.com>
2024-05-02 07:48:43 +0700

    api

--- app/View/Components/Personil.php
+++ app/View/Components/Personil.php
@@ -65,7 +65,7 @@ class Personil extends Component
 
         $opdId = env('ID_OPD'); // Mengambil nilai OPD_ID dari variabel lingkungan
 
-        $response = Http::withoutVerifying()->get('https://api.wonosobokab.go.id/api/list-personild/' . $opdId);
+        $response = Http::withoutVerifying()->get('https://api.wonosobokab.go.id/api/list-personil/' . $opdId);
 
         if (!$response->successful()) {
             return '<center><h1>Koneksi Gagal...</h1></center>';
