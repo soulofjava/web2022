b1de134e66fa46849fd324d1e1b137f03e621e7f
isamaulanatantra <isamaulanatantra@gmail.com>
2024-05-07 11:25:27 +0700

    asdas

--- app/View/Components/Personil.php
+++ app/View/Components/Personil.php
@@ -67,6 +67,8 @@ class Personil extends Component
 
         $response = Http::withoutVerifying()->get('https://api.wonosobokab.go.id/api/list-personil/' . $opdId);
 
+        return $response->body();
+
         if (!$response->successful()) {
             return '<center><h1>Koneksi Gagal...</h1></center>';
         }
