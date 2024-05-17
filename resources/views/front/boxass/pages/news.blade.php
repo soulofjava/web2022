96cc546b225e9b880c67ab784946dee57928e751
isamaulanatantra <isamaulanatantra@gmail.com>
2024-05-17 07:24:51 +0700

    hide img

--- resources/views/front/boxass/pages/news.blade.php
+++ resources/views/front/boxass/pages/news.blade.php
@@ -36,9 +36,9 @@
                             <div class="thumb">
                                 <a href="{{ url('/news-detail', $n->slug) }}">
                                     @if(Storage::get($n->gambarmuka->path ?? ''))
-                                    <img src="{{ route('helper.show-picture', ['path' => $n->gambarmuka->path]) }}"
+                                    <!-- <img src="{{ route('helper.show-picture', ['path' => $n->gambarmuka->path]) }}"
                                         class="img-fluid" alt="{{ $n->gambarmuka->file_name }}"
-                                        style="background-size: cover; height: 240px; background-position: center; width: 100%;">
+                                        style="background-size: cover; height: 240px; background-position: center; width: 100%;"> -->
                                     @else
                                     <!-- <img src="{{ asset('img/soulofjava.jpg') }}" class="img-fluid" alt="soul of java"> -->
                                     @endif
