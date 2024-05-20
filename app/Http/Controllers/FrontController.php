913e1ed65ccc47f62bbe6edde74bd2d46510d378
isamaulanatantra <isamaulanatantra@gmail.com>
2024-05-20 12:41:10 +0700

    berita

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -241,7 +241,7 @@ class FrontController extends Controller
         Seo::seO();
         $cari = $request->kolomcari;
         $hasil = 'Search result : ' . $cari;
-        $data = News::with('gambar')->whereDate('date', 'like', '%' . $cari . '%')->orWhere('title', 'like', '%' . $cari . '%')->orderBy("date", "desc")->paginate();
+        $data = News::with('gambarmuka')->whereDate('date', 'like', '%' . $cari . '%')->orWhere('title', 'like', '%' . $cari . '%')->latest("date")->paginate();
         $news = News::latest('date')->take(5)->get();
         return view('front.pages.newsbyauthor', compact('data', 'news', 'hasil'));
     }
@@ -249,7 +249,7 @@ class FrontController extends Controller
     public function newsall(Request $request)
     {
         Seo::seO();
-        $news = News::whereIn('kategori', ['berita', 'informasi umum'])->latest('date')->paginate(5);
+        $news = News::with('gambarmuka')->whereIn('tag', ['berita', 'informasi umum'])->latest('date')->paginate(5);
         $sideposts = News::latest('date')->take(5)->get();
         return view('front.pages.news', compact('news', 'sideposts'));
     }
