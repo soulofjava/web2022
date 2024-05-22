d8097d49f35e5ef81a365cab7ad128850e480087
isamaulanatantra <isamaulanatantra@gmail.com>
2024-05-22 14:16:28 +0700

    foto pejabat

--- resources/views/components/kecamatan.blade.php
+++ resources/views/components/kecamatan.blade.php
@@ -1,10 +1,12 @@
 <div>
     <!-- Very little is needed to make a happy life. - Marcus Aurelius -->
-</div><div>
-    <div class="container">
+</div>
+<div>
+    <div class="container" style="overflow-x: scroll;">
         <table class="table mt-4 table-striped table-bordered table-hover">
             <thead class="thead-dark">
                 <tr>
+                    <th style="text-align: center;">FOTO</th>
                     <th style="text-align: center;">NAMA</th>
                     <th style="text-align: center;">PANGKAT / GOL. RUANG</th>
                     <th style="text-align: center;">JABATAN</th>
@@ -14,6 +16,10 @@
             <tbody>
                 @foreach ($struktural as $list)
                 <tr>
+                    <td>
+                        <img src="https://simpeg.wonosobokab.go.id/packages/upload/photo/pegawai/{{ $list['photo'] }}"
+                            loading="lazy" alt="fotonya" width="100px">
+                    </td>
                     <td>{{ $list['gdp'].$list['nama'].', '.$list['gdb'] }}</td>
                     <td>{{ $list['pangkat'].' / '.$list['golru'] }}</td>
                     <td>{{ $list['jabatan'] }}</td>
--- resources/views/components/sekretariat-daerah.blade.php
+++ resources/views/components/sekretariat-daerah.blade.php
@@ -1,8 +1,9 @@
 <div>
-    <div class="container">
+    <div class="container" style="overflow-x: scroll;">
         <table class="table mt-4 table-striped table-bordered table-hover">
             <thead class="thead-dark">
                 <tr>
+                    <th style="text-align: center;">FOTO</th>
                     <th style="text-align: center;">NAMA</th>
                     <th style="text-align: center;">PANGKAT / GOL. RUANG</th>
                     <th style="text-align: center;">JABATAN</th>
@@ -12,6 +13,10 @@
             <tbody>
                 @foreach ($struktural as $list)
                 <tr>
+                    <td>
+                        <img src="https://simpeg.wonosobokab.go.id/packages/upload/photo/pegawai/{{ $list['photo'] }}"
+                            loading="lazy" alt="fotonya" width="100px">
+                    </td>
                     <td>{{ $list['gdp'].$list['nama'].', '.$list['gdb'] }}</td>
                     <td>{{ $list['pangkat'].' / '.$list['golru'] }}</td>
                     <td>{{ $list['jabatan'] }}</td>
