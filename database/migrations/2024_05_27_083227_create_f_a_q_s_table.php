c77ed07576c0f76692f969bb12e6a9e50e7f9638
isamaulanatantra <isamaulanatantra@gmail.com>
2024-05-27 13:08:53 +0700

    ubah ke long text

--- database/migrations/2024_05_27_083227_create_f_a_q_s_table.php
+++ database/migrations/2024_05_27_083227_create_f_a_q_s_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('f_a_q_s', function (Blueprint $table) {
             $table->id();
-            $table->string('pertanyaan');
-            $table->string('jawaban');
+            $table->longText('pertanyaan');  
+            $table->longText('jawaban'); 
             $table->timestamps();
         });
     }
