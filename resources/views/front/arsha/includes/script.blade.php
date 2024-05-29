ac26823cc19d61de39ed1c1120558007d26519db
isamaulanatantra <isamaulanatantra@gmail.com>
2024-05-29 13:44:18 +0700

    disa

--- resources/views/front/arsha/includes/script.blade.php
+++ resources/views/front/arsha/includes/script.blade.php
@@ -14,3 +14,5 @@
 <!-- DataTables   -->
 <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
+<!-- accessibility widget -->
+<script src="https://website-widgets.pages.dev/dist/sienna.min.js" defer></script>
