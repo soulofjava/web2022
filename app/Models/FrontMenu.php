a2b2a68ad3cc1dba3810eea66f8eda6274d7f46a
isamaulanatantra <isamaulanatantra@gmail.com>
2024-05-29 13:28:44 +0700

    model

--- app/Models/FrontMenu.php
+++ app/Models/FrontMenu.php
@@ -14,6 +14,8 @@ class FrontMenu extends Model
     public function getContentAttribute($value)
     {
         // Ganti URL 'src' menggunakan regular expression
+        //ubah juga ketika ada href="/storage/photos/
+        $newValue = str_replace('src="/storage/photos/', 'src="' . url('show-picture?path=photos/'), $value);
         $newValue = str_replace('href="/storage/files/', 'href="' . url('show-picture?path=files/'), $value);
         return $newValue;
     }
