39a58a153bc92607c5d6eeeb997bc30597548aba
isamaulanatantra <isamaulanatantra@gmail.com>
2024-05-29 13:30:27 +0700

    h

--- app/Models/FrontMenu.php
+++ app/Models/FrontMenu.php
@@ -16,7 +16,7 @@ class FrontMenu extends Model
         // Ganti URL 'src' menggunakan regular expression
         //ubah juga ketika ada href="/storage/photos/
         $newValue = str_replace('src="/storage/photos/', 'src="' . url('show-picture?path=photos/'), $value);
-        $newValue = str_replace('href="/storage/files/', 'href="' . url('show-picture?path=files/'), $value);
+        $newValue = str_replace('href="/storage/files/', 'href="' . url('show-picture?path=files/'), $newValue);
         return $newValue;
     }
 
