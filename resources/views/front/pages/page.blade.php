135d65c4b14b813f1f4b48fb18ddf463864dfc02
isamaulanatantra <isamaulanatantra@gmail.com>
2024-06-10 07:23:55 +0700

    scroll

--- resources/views/front/pages/page.blade.php
+++ resources/views/front/pages/page.blade.php
@@ -6,7 +6,7 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                <div class="blog-details-wrap">
+                <div class="blog-details-wrap" style="overflow-x: scroll;">
                     @if($data->menu_name == 'Daftar Informasi Publik')
                     <x-jip />
                     @elseif($data->menu_name == 'Personil')
