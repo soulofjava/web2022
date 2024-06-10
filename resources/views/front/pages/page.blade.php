368a2bc39638a3384febca5c51e398987bdf0bcd
isamaulanatantra <isamaulanatantra@gmail.com>
2024-06-10 07:27:06 +0700

    asd

--- resources/views/front/pages/page.blade.php
+++ resources/views/front/pages/page.blade.php
@@ -1,12 +1,11 @@
-
 @extends('front.layouts.app')
 @section('content')
 <!-- Blog Details Start -->
-<section class="blog-details-area py-130 rpy-100" style="height: 100vh; overflow-y: auto;">
+<section class="blog-details-area py-130 rpy-100" style="height: 100vh; overflow-y: auto; overflow-x: scroll;">
     <div class="container">
         <div class="row">
             <div class="col">
-                <div class="blog-details-wrap" style="overflow-x: scroll;">
+                <div class="blog-details-wrap">
                     @if($data->menu_name == 'Daftar Informasi Publik')
                     <x-jip />
                     @elseif($data->menu_name == 'Personil')
@@ -29,14 +28,13 @@
 @push('after-script')
 <script>
     $.get('{{ route('api.personil') }}', function (data) {
-            // Lakukan sesuatu dengan data yang diterima
-            console.log(data);
-            $('#personil').empty();
-            $('#personil').html(data);
-        }).fail(function (xhr, status, error) {
-            // Tangani kesalahan jika ada
-            console.error(xhr.responseText);
-        });
-
+        // Lakukan sesuatu dengan data yang diterima
+        console.log(data);
+        $('#personil').empty();
+        $('#personil').html(data);
+    }).fail(function (xhr, status, error) {
+        // Tangani kesalahan jika ada
+        console.error(xhr.responseText);
+    });
 </script>
 @endpush
