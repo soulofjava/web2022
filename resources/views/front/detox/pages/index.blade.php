9eab2cfd4ffc36d300a5b2f3782829a81373a0a3
isamaulanatantra <isamaulanatantra@gmail.com>
2024-06-12 12:03:37 +0700

    if bagian faq

--- resources/views/front/detox/pages/index.blade.php
+++ resources/views/front/detox/pages/index.blade.php
@@ -18,15 +18,13 @@
                 <div class="row clearfix">
                     <div class="col-lg-6 col-md-12 col-sm-12 content-column">
                         <div class="content-box wow fadeInUp" data-wow-delay="0ms" data-wow-duration="1500ms">
-                            <h1>{!!
-                                \Illuminate\Support\Str::limit($hl->title, 10,
-                                $end='...') !!}</h1>
-                            <p>{!!
-                                \Illuminate\Support\Str::limit($hl->content, 50,
-                                $end='...') !!}</p>
+                            <h1>
+                                {!! \Illuminate\Support\Str::limit($hl->title, 45, $end='...') !!}
+                            </h1>
                             <div class="btn-box">
-                                <a href="{{ url('/news-detail', $hl->slug) }}" class="theme-btn style-one">Lebih
-                                    Detail</a>
+                                <a href="{{ url('/news-detail', $hl->slug) }}" class="theme-btn style-one">
+                                    Lebih Detail
+                                </a>
                             </div>
                         </div>
                     </div>
@@ -38,7 +36,7 @@
                                 <img loading="lazy"
                                     src="{{ route('helper.show-picture', ['path' => $hl->gambarmuka->path]) }}"
                                     alt="thumb"
-                                    style="width: 687px !important; height: 612px !important; object-fit: cover !important;">
+                                    style="width: 600px !important; height: 612px !important; object-fit: cover !important;">
                                 @else
                                 <img loading="lazy" src="{{ asset('master/Detox/assets/images/banner/banner-1.png') }}"
                                     alt="thumb">
@@ -53,35 +51,38 @@
     </div>
 </section>
 
+@if(App\Models\FAQ::count())
 <!-- faq-section -->
 <section class="faq-section faq-page-section sec-pad bg-color-1">
     <div class="auto-container">
         <div class="sec-title text-center">
             <h2>Pertanyaan yang Sering Diajukan</h2>
             <div class="text">Berikut adalah pertanyaan yang sering diajukan oleh pengunjung</div>
-        <div class="row clearfix">
-            <div class="col-lg-12 col-md-12 col-sm-12 inner-column">
-                <div id="content_block_07">
-                    <ul class="accordion-box">
-                        @foreach(App\Models\FAQ::all() as $faq)
-                        <li class="accordion block">
-                            <div class="acc-btn">
-                                <h4><span>Q.</span>{{ $faq->pertanyaan }}</h4>
-                            </div>
-                            <div class="acc-content">
-                                <div class="content">
-                                    <p class="abcde">{!! $faq->jawaban !!}</p>
+            <div class="row clearfix">
+                <div class="col-lg-12 col-md-12 col-sm-12 inner-column">
+                    <div id="content_block_07">
+                        <ul class="accordion-box">
+                            @foreach(App\Models\FAQ::all() as $faq)
+                            <li class="accordion block">
+                                <div class="acc-btn">
+                                    <h4><span>Q.</span>{{ $faq->pertanyaan }}</h4>
                                 </div>
-                            </div>
-                        </li>
-                        @endforeach
-                    </ul>
+                                <div class="acc-content">
+                                    <div class="content">
+                                        <p class="abcde">{!! $faq->jawaban !!}</p>
+                                    </div>
+                                </div>
+                            </li>
+                            @endforeach
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 </section>
 <!-- faq-section end -->
+@endif
+
 @endsection
 @push('after-style')
 <style>
